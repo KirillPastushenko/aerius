896bd37732764360bd531f0648bbc3667a49c67d
KirillPastushenko <kpastushenko@gmail.com>
2020-08-08 08:11:05 +0300

    -

--- src/components/Heptagons.vue
+++ src/components/Heptagons.vue
@@ -1,6 +1,27 @@
 <template>
-    <div>
-        <div v-for="(item, index) in filteredHeptagons"
+    <div v-if="switchSwiper">
+
+        <swiper class="swiper" :options="swiperOption">
+            <swiper-slide v-for="(item, index) in getHeptagons" :key="index">
+                <svg :data-id="item.id" :class="`heptagon-type${item.type}`" viewBox="0 0 249 242" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2">
+                    <use class="heptagon1" xlink:href="#heptagon1"/>
+                    <use class="heptagon2" xlink:href="#heptagon2"/>
+                    <path class="icon" :d="item.icon"  />
+                </svg>
+                <div class="desc">
+                    <h3 class="red angle-tl angle-br">{{item.title}}</h3>
+                    <p v-html="item.description"></p>
+                </div>
+            </swiper-slide>
+            <div class="swiper-button-prev" slot="button-prev"></div>
+            <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+
+    </div>
+    <div v-else>
+
+
+        <div v-for="(item, index) in getHeptagons"
             :class="{active: activeItem === index}" 
             :key="index"
             @mouseover="activeItem = index">
@@ -10,49 +31,59 @@
                 <path class="icon" :d="item.icon"  />
             </svg>
             <div class="desc">
-                <h3 class="red angle-tl angle-br">{{ item.title }}</h3>
+                <h3 class="red angle-tl angle-br">{{item.title}}</h3>
                 <p v-html="item.description"></p>
             </div>
         </div>
     </div>
+
 </template>
+ 
 
 <script>
     import * as heptagonsData from './heptagons-data';
-    import { Swiper, Navigation, Pagination} from 'swiper';
-    import 'swiper/swiper-bundle.min.css';
+    import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
+     
+    
     export default {
-        props: ['group'],
+        props: ['group', 'swiper'],
         data(){
             return {
+                width: 0,
+                isSwiperActive: false,
                 activeItem: 0,
                 items: heptagonsData.heptagonsData,
-                swiperGroupIDs: [1,3,4]
-            }
-        }, 
-        computed: {
-            filteredHeptagons: function () {
-                return this.items.filter(item => { 
-                    return  this.group === item.group;
-                })
-            }
-        },
-        mounted() {
-          // if(mob(768)){
-                const heptagonsSwiper1 = new Swiper('#heptagons1>div', {
+                swiperOption: {
                     loop: true,
                     slidesPerView: 3,
-                    spaceBetween: 10,
-                    pagination: {
-                        el: '.swiper-pagination',
-                        clickable: true
-                    },
                     navigation: {
                         nextEl: '.swiper-button-next',
                         prevEl: '.swiper-button-prev'
-                    } 
-                });
-          //  };
+                    }
+                }
+            }
+        }, 
+        methods: {
+            updateWidth() {
+                this.width = window.innerWidth;
+            }
+        },
+        computed: {
+            getHeptagons: function () {
+                return this.items.filter(item => { 
+                    return this.group === item.group;
+                })
+            },
+            switchSwiper: function() {
+                return this.width <= 768 && this.swiper === '1'
+            }
+        },
+        created() {
+            window.addEventListener('resize', this.updateWidth);
+        },
+        components: {
+            Swiper,
+            SwiperSlide
         }
     }
 </script>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-//window.Vue = require('vue');
 import Vue from 'vue';
-//import Heptagons from './components/Heptagons.vue';
 import lottie from "lottie-web";
 import { gsap, TimelineMax, TweenMax, Linear } from 'gsap';
 import ScrollMagic from 'scrollmagic';
 import ScrollMagicGSAP from './js/animation.gsap';
-
-//import VueAwesomeSwiper from 'vue-awesome-swiper'
-//import 'swiper/swiper-bundle.css'
-import { Swiper, Navigation, Pagination} from 'swiper';
-import 'swiper/swiper-bundle.min.css';
 import './css/main.css';
 
 
@@ -17,80 +10,69 @@ var animData = {
     container: document.getElementById('cells'),
     path: 'https://assets8.lottiefiles.com/packages/lf20_wJH0k2.json',
     renderer: 'svg',
-    loop: false, 
+    loop: false,
     autoplay: false,
     name: 'animScroll',
 }, animScroll, tl;
- 
+
 var animScroll = lottie.loadAnimation(animData);
 
 animScroll.addEventListener('DOMLoaded', function () {
-    tl = new TimelineMax({repeat: 0})
-    tl.to({frame: 0}, 1, {
+    tl = new TimelineMax({ repeat: 0 })
+    tl.to({ frame: 0 }, 1, {
         frame: animScroll.totalFrames - 1,
-        onUpdate: function() {
+        onUpdate: function () {
             animScroll.goToAndStop(Math.round(this._targets[0].frame), true)
         },
-        Ease:Linear.easeNone
+        Ease: Linear.easeNone
     })
     var controller = new ScrollMagic.Controller();
     var scene = new ScrollMagic.Scene({
         triggerElement: '.cells-wrap',
         offset: 300,
-        duration: 5770 })
-            .setTween(tl)
-            .setPin('#cells')
-            .addTo(controller); 
+        duration: 5770
+    })
+        .setTween(tl)
+        .setPin('#cells')
+        .addTo(controller);
 });
 
- 
-Vue.component('Heptagons',require('./components/Heptagons.vue').default);
 
-Swiper.use([Navigation, Pagination]);
+Vue.component('Heptagons', require('./components/Heptagons.vue').default);
 
-const mob = (res) => res >= document.body.clientWidth;  
+
+
+const app = new Vue({
+    el: '#app',
+    data: function () {
+        return {
+            activeClass: 'nose'
+        }
+    },
+    methods: {
+        setActiveClass: function (className) {
+            if (this.activeClass != className) {
+                this.activeClass = className;
+                gsap.set('[class^=dot-]', { clearProps: 'transform' });
+                gsap.to(`.dot-${className}`, { scale: 2, strokeWidth: '2.2', transformOrigin: '50% 50%', duration: .3 });
+            }
+        }
+    }
+});
+
+
+const mob = (res) => res >= document.body.clientWidth;
+
 const elementInViewport = (el) => {
     var bounds = el.getBoundingClientRect();
     return ([(bounds.top + bounds.height > 0) && (window.innerHeight - bounds.top > 0), bounds]);
 }
 
 
-const app = new Vue({
-    el: '#app',
-    data: function () {
-        return { 
-            activeClass:'nose'
-        }
-    },
-    methods:{
-        setActiveClass: function(className){
-            if(this.activeClass != className){
-                this.activeClass = className;
-                gsap.set('[class^=dot-]', { clearProps: 'transform' });
-                gsap.to(`.dot-${className}`, {scale: 2, strokeWidth: '2.2', transformOrigin: '50% 50%', duration: .3});
-            }
-        }
-    },
-    mounted() {
-        const sovetySwiper = new Swiper('.swiper-container', {
-            loop: true,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev'
-            } 
-        });
-    }
-}); 
+gsap.to('.circles-red circle:nth-of-type(2n)', { scale: 1.3, transformOrigin: "50% 50%", duration: .3, repeat: -1 });
+gsap.to('.circles-red circle:nth-of-type(2n+1)', { scale: 1.4, transformOrigin: "50% 50%", duration: .4, repeat: -1 });
+gsap.to('.circles-red circle:nth-of-type(3n)', { scale: 1.5, transformOrigin: "50% 50%", duration: .5, repeat: -1 });
 
-
-gsap.to('.circles-red circle:nth-of-type(2n)', {scale:1.3,transformOrigin: "50% 50%", duration:.3, repeat:-1});
-gsap.to('.circles-red circle:nth-of-type(2n+1)', {scale:1.4,transformOrigin: "50% 50%", duration:.4, repeat:-1});
-gsap.to('.circles-red circle:nth-of-type(3n)', {scale:1.5,transformOrigin: "50% 50%", duration:.5, repeat:-1});
-
- const tl2 = gsap.timeline({});
- tl2.staggerTo("#top-head path", 5, {display:'initial',ease:'CubicIn.ease'}, 0.0025);
+const tl2 = gsap.timeline({});
+tl2.staggerTo("#top-head path", 5, { display: 'initial', ease: 'CubicIn.ease' }, 0.0025);
 
